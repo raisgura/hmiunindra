59d31a977b6c425da1c65469e94126f379ac1143
owl <154574205+raisgura@users.noreply.github.com>
2024-01-04 05:04:26 +0700

    Delete data.sql

--- data.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE contact_submissions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    subject VARCHAR(255) NOT NULL,
-    message TEXT NOT NULL,
-    submission_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
